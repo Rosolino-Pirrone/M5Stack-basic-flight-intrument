02f6a35c95e67834de13470e59e4ded8c914e565
Rosolino Pirrone <rosolinopirrone@libero.it>
2022-04-23 19:25:46 +0200

    OTA Update
    OTA Update

--- M5Stack_vario_menu_altimetri_blue_bleuart.ino
+++ M5Stack_vario_menu_altimetri_blue_bleuart.ino
@@ -8,6 +8,16 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+const char* ssid = "Ospiti-TIM-32862941";
+const char* password = "guest@000000";
 
 BLEServer *pServer = NULL;
 BLECharacteristic *pTxCharacteristic;
@@ -218,6 +228,10 @@ void readFile(fs::FS &fs, const char * path) {
   file.close();
 }
 
+
+
+
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////        setup        //////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -249,7 +263,7 @@ void setup() {
   bluetooth = EEPROM.read(6);
   delay(125);
 
-  //if (suono == 1) {
+  if (suono == 1) {
     ledcWriteTone(channel, 1000);
     delay(125);
     ledcWriteTone(channel, 0);
@@ -259,7 +273,7 @@ void setup() {
     ledcWriteTone(channel, 500);
     delay(125);
     ledcWriteTone(channel, 0);
- //}
+  }
 
   if (bluetooth == true) {
     Serial.println("The device started, now you can pair it with bluetooth!");
@@ -405,10 +419,50 @@ void setup() {
 
   //Serial.print(" C_6: ");
   //Serial.println(C_6);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
 
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
   delay(1000);
 
 }
+
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////       loop       ////////////////////////////////////////////////
@@ -432,7 +486,7 @@ void loop() {
 
 
   //Stringhe di simulazione FIX GPS, in questo modo si può provare lo sketch
- // NMEA_GGA = "$GNGGA,101542.00,3754.20531,N,01325.61218,E,1,07,1.11,727.7,M,38.8,M,,*48\n";
+  //NMEA_GGA = "$GNGGA,101542.00,3754.20531,N,01325.61218,E,1,07,1.11,727.7,M,38.8,M,,*48\n";
   //NMEA_RMC = "$GNRMC,101542.00,A,3754.20531,N,01325.61218,E,7,225.98,190422,,,A,*5a\n";
 
 
@@ -1086,7 +1140,7 @@ void loop() {
             readFile(SD, newFile.c_str());
             while (1) {
               M5.update();
-
+             
               if (M5.BtnB.wasReleased()) {
                 M5.Lcd.fillScreen(TFT_BLACK);
                 M5.Lcd.setTextSize(2);
@@ -1098,6 +1152,65 @@ void loop() {
 
           break;
 
+          case 8:
+          M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+          M5.Lcd.setCursor(0, 0);
+          M5.Lcd.print("Impostazioni");
+          M5.Lcd.setCursor(0, 25);
+          M5.Lcd.print("Sound");
+          M5.Lcd.setCursor(200, 25);
+          if (suono == true) {
+            M5.Lcd.print("On ");
+          } else M5.Lcd.print("Off");
+          M5.Lcd.setCursor(0, 50);
+          M5.Lcd.print("Vario +");
+          M5.Lcd.setCursor(200, 50);
+          M5.Lcd.print(soglia_pos);
+          M5.Lcd.setCursor(250, 50);
+          M5.Lcd.print("cm");
+          M5.Lcd.setCursor(0, 75);
+          M5.Lcd.print("Vario -");
+          M5.Lcd.setCursor(200, 75);
+          M5.Lcd.print(soglia_neg);
+          M5.Lcd.setCursor(250, 75);
+          M5.Lcd.print("cm");
+          M5.Lcd.setCursor(0, 100);
+          M5.Lcd.print("Media");
+          M5.Lcd.setCursor(200, 100);
+          M5.Lcd.print(media);
+          M5.Lcd.setCursor(0, 125);
+          M5.Lcd.print("Bluetooth");
+          M5.Lcd.setTextColor(TFT_RED, TFT_BLACK);
+          M5.Lcd.setCursor(0, 150);
+          M5.Lcd.print("Arduvario OTA");
+          Serial.println("Arduvario OTA");
+          M5.Lcd.setTextColor(TFT_GREEN, TFT_BLACK);
+          M5.Lcd.setCursor(125, 220);
+          M5.Lcd.print("On/Off");
+          M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+          M5.Lcd.setCursor(200, 125);
+          if (bluetooth == true) {
+            M5.Lcd.print("On ");
+          } else M5.Lcd.print("Off");
+
+          if (M5.BtnB.wasReleased()) {
+            M5.Lcd.fillScreen(TFT_BLACK);
+            termInit();
+            M5.Lcd.setTextSize(1);
+            
+            while (1) {
+              M5.update();
+              ArduinoOTA.handle();
+
+              if (M5.BtnB.wasReleased()) {
+                M5.Lcd.fillScreen(TFT_BLACK);
+                M5.Lcd.setTextSize(2);
+                M5.Lcd.begin();
+                break;
+              }
+            }
+          }
+
 
 
         default:
@@ -1108,7 +1221,7 @@ void loop() {
 
       if (M5.BtnA.wasReleased()) {
         element++;
-        if (element > 7) element = 0;
+        if (element > 8) element = 0;
         if (element == 0)M5.Speaker.tone(350, 200);
         else M5.Speaker.beep();
       } else if (M5.BtnB.wasReleased()) {
